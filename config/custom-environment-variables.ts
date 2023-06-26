b885ea9180eb9048aed7ebe7f0cfb5cb35d47253
Calvin Tiley <calvintiley@hotmail.co.uk>
2023-06-26 18:07:14 +0100

    feat: cors implementation

--- config/custom-environment-variables.ts
+++ config/custom-environment-variables.ts
@@ -3,4 +3,5 @@ export default {
     nodeEnv: "NODE_ENV",
     jwtSecret: "JWT_ACCESS_SECRET",
     jwtRefreshSecret: "JWT_REFRESH_SECRET",
+    clientOrigin: "CLIENT_ORIGIN",
 };
--- src/middleware/server.ts
+++ src/middleware/server.ts
@@ -1,6 +1,13 @@
 import express, { Express } from "express";
+import cors from "cors";
+import config from "config";
 
 export const serverMiddleware = (app: Express) => {
+    app.use(cors({
+        origin: config.get("clientOrigin"),
+        optionsSuccessStatus: 200,
+    }));
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 };
